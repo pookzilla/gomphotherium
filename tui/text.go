4bef00aef9d0cfe8447bbda50bd7dbd7a5ab84f3
Kimberly Horne <kim.horne@gmail.com>
2022-11-13 21:44:42 +0000

    unicode support

--- tui/text.go
+++ tui/text.go
@@ -57,13 +57,14 @@ func WrapWithIndent(
 	readWidth := 0
 	out := indentString
 	word := ""
-	for _, r := range stringToWrap {
-		characterWidth := runewidth.RuneWidth(r)
-		if r == '\n' || r == '\r' {
+	for _, character := range stringToWrap {
+		characterWidth := runewidth.RuneWidth(character)
+		if character == '\n' || character == '\r' {
+			wordList.PushBack(word)
 			out += DrainLine(*wordList, 0, 0)
 
-			out += word
-			out += string('\n')
+			// out += word
+			out += "\n"
 			out += indentString
 
 			word = ""
@@ -71,8 +72,7 @@ func WrapWithIndent(
 			spaceCount = 0
 			committedCharacterCount = 0
 			readWidth = 0
-			continue
-		} else if r == ' ' {
+		} else if character == ' ' {
 			wordList.PushBack(word)
 			wordList.PushBack(StringPtr(" "))
 			spaceCount++
@@ -82,39 +82,30 @@ func WrapWithIndent(
 
 			word = ""
 			readWidth += characterWidth
-			continue
 		} else if readWidth + characterWidth > maximumWidth {
-			characterCountOfCurrentWord += characterWidth
 			// giant word - we need to print part of it
-			if characterCountOfCurrentWord >= maximumWidth {
-				wordList.PushBack(word[:maximumWidth-1])
-				word = word[maximumWidth-1:]
-				characterCountOfCurrentWord = characterWidth
+			if characterCountOfCurrentWord + characterWidth >= maximumWidth {
+				wordList.PushBack(word)
+				word = ""
 			}
+			readWidth = characterWidth
+			characterCountOfCurrentWord = characterWidth //characterWidth
+			word += string(character)
 
 			leftoverSpace := 0
 			if justifyText {
 				leftoverSpace = int(math.Min(float64(maximumWidth-(committedCharacterCount + spaceCount)), float64(spaceCount) * 1.5))
 			}
 			out += DrainLine(*wordList, spaceCount, leftoverSpace)
-
 			out += "\n"
 			out += indentString
 			spaceCount = 0
 			committedCharacterCount = characterCountOfCurrentWord
-			readWidth = characterCountOfCurrentWord
 			wordList = list.New()
-
-			word += string(r)
-
-			readWidth += characterWidth
-			continue
-
 		} else {
 			readWidth += characterWidth
-			word += string(r)
 			characterCountOfCurrentWord += characterWidth
-			continue
+			word += string(character)
 		}
 	}
 	out += DrainLine(*wordList, 0, 0)
