0e655651e100a2ebff8aef235f1dc584cdfdf6ac
マリウス <marius@xn--gckvb8fzb.com>
2022-11-20 15:17:48 -0500

    Bowwed to Go's formatting dictature ... so that others interested in this have an easier time contributing to the project.

--- cli/rootCmd.go
+++ cli/rootCmd.go
@@ -1,11 +1,11 @@
 package cli
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 
-  "github.com/spf13/cobra"
-  "github.com/mattn/go-mastodon"
+	"github.com/mattn/go-mastodon"
+	"github.com/spf13/cobra"
 )
 
 var help string
@@ -21,53 +21,53 @@ var flagJustifyText bool
 var MastodonClient *mastodon.Client
 
 var rootCmd = &cobra.Command{
-  Use:   "gomphotherium",
-  Short: "Command line Mastodon client",
-  Long:  `A command line client for Mastodon.`,
+	Use:   "gomphotherium",
+	Short: "Command line Mastodon client",
+	Long:  `A command line client for Mastodon.`,
 }
 
 func Execute(embeddedHelp string) {
-  help = embeddedHelp
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Printf("%+v\n", err)
-    os.Exit(-1)
-  }
+	help = embeddedHelp
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(-1)
+	}
 }
 
 func init() {
-  cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig)
 
-  rootCmd.PersistentFlags().StringVar(
-    &server,
-    "server",
-    LookupStrEnv("GOMPHOTHERIUM_SERVER",""),
-    "Mastodon server",
-  )
-  rootCmd.PersistentFlags().StringVar(
-    &accessToken,
-    "access-token",
-    LookupStrEnv("GOMPHOTHERIUM_ACCESS_TOKEN",""),
-    "Mastodon access token",
-  )
-  rootCmd.PersistentFlags().BoolVarP(
-    &flagShowImages,
-    "show-images",
-    "i",
-    true,
-    "Show images in timeline",
-  )
-  rootCmd.PersistentFlags().BoolVarP(
-    &flagJustifyText,
-    "justify-text",
-    "j",
-    true,
-    "Justify text in timeline",
-  )
+	rootCmd.PersistentFlags().StringVar(
+		&server,
+		"server",
+		LookupStrEnv("GOMPHOTHERIUM_SERVER", ""),
+		"Mastodon server",
+	)
+	rootCmd.PersistentFlags().StringVar(
+		&accessToken,
+		"access-token",
+		LookupStrEnv("GOMPHOTHERIUM_ACCESS_TOKEN", ""),
+		"Mastodon access token",
+	)
+	rootCmd.PersistentFlags().BoolVarP(
+		&flagShowImages,
+		"show-images",
+		"i",
+		true,
+		"Show images in timeline",
+	)
+	rootCmd.PersistentFlags().BoolVarP(
+		&flagJustifyText,
+		"justify-text",
+		"j",
+		true,
+		"Justify text in timeline",
+	)
 }
 
 func initConfig() {
-  MastodonClient = mastodon.NewClient(&mastodon.Config{
-    Server:       server,
-    AccessToken:  accessToken,
-  })
+	MastodonClient = mastodon.NewClient(&mastodon.Config{
+		Server:      server,
+		AccessToken: accessToken,
+	})
 }
--- tui/text.go
+++ tui/text.go
@@ -1,125 +1,123 @@
 package tui
 
 import (
-  "container/list"
-  "github.com/mattn/go-runewidth"
-  "math"
-  "math/rand"
+	"container/list"
+	"github.com/mattn/go-runewidth"
+	"math"
+	"math/rand"
 )
 
 func StringPtr(s string) *string {
-  return &s
+	return &s
 }
 
 func DrainLine(
-  wordList list.List,
-  numberOfSpaces int,
-  leftoverSpaces int) string {
+	wordList list.List,
+	numberOfSpaces int,
+	leftoverSpaces int) string {
 
-  out := ""
-  spaceList := make([]*string, 0)
-  for e := wordList.Front(); e != nil; e = e.Next() {
-    value, ok := e.Value.(*string)
-    if ok {
-      spaceList = append(spaceList, value)
-    }
-  }
+	out := ""
+	spaceList := make([]*string, 0)
+	for e := wordList.Front(); e != nil; e = e.Next() {
+		value, ok := e.Value.(*string)
+		if ok {
+			spaceList = append(spaceList, value)
+		}
+	}
 
-  for i := leftoverSpaces; i > 0; i-- {
-    selection := rand.Intn(numberOfSpaces)
-    *spaceList[selection] += " "
-  }
+	for i := leftoverSpaces; i > 0; i-- {
+		selection := rand.Intn(numberOfSpaces)
+		*spaceList[selection] += " "
+	}
 
-  for e := wordList.Front(); e != nil; e = e.Next() {
-    value, ok := e.Value.(string)
-    if ok {
-      out += value
-    } else {
-      value, ok := e.Value.(*string)
-      if ok {
-        out += *value
-      }
-    }
-  }
-  return out
+	for e := wordList.Front(); e != nil; e = e.Next() {
+		value, ok := e.Value.(string)
+		if ok {
+			out += value
+		} else {
+			value, ok := e.Value.(*string)
+			if ok {
+				out += *value
+			}
+		}
+	}
+	return out
 }
 
 func WrapWithIndent(
-  stringToWrap string,
-  maximumWidth int,
-  indentString string,
-  justifyText bool,
+	stringToWrap string,
+	maximumWidth int,
+	indentString string,
+	justifyText bool,
 ) string {
 
-  wordList := list.New()
+	wordList := list.New()
 
-  spaceCount := 0
-  committedCharacterCount := 0
-  characterCountOfCurrentWord := 0
+	spaceCount := 0
+	committedCharacterCount := 0
+	characterCountOfCurrentWord := 0
 
-  out := indentString
-  word := ""
-  for _, character := range stringToWrap {
-    characterWidth := runewidth.RuneWidth(character)
-    if character == '\n' || character == '\r' {
-      if (
-        characterCountOfCurrentWord + committedCharacterCount >= maximumWidth) {
-        out += "\n"
-        out += indentString
-      }
-      wordList.PushBack(word)
-      line := DrainLine(*wordList, 0, 0)
-      out += line
-      out += "\n"
-      out += indentString
+	out := indentString
+	word := ""
+	for _, character := range stringToWrap {
+		characterWidth := runewidth.RuneWidth(character)
+		if character == '\n' || character == '\r' {
+			if characterCountOfCurrentWord+committedCharacterCount >= maximumWidth {
+				out += "\n"
+				out += indentString
+			}
+			wordList.PushBack(word)
+			line := DrainLine(*wordList, 0, 0)
+			out += line
+			out += "\n"
+			out += indentString
 
-      word = ""
-      characterCountOfCurrentWord = 0
-      wordList = list.New()
-      spaceCount = 0
-      committedCharacterCount = 0
-    } else if character == ' ' {
-      wordList.PushBack(word)
-      wordList.PushBack(StringPtr(" "))
-      spaceCount++
-      committedCharacterCount += characterCountOfCurrentWord + 1
+			word = ""
+			characterCountOfCurrentWord = 0
+			wordList = list.New()
+			spaceCount = 0
+			committedCharacterCount = 0
+		} else if character == ' ' {
+			wordList.PushBack(word)
+			wordList.PushBack(StringPtr(" "))
+			spaceCount++
+			committedCharacterCount += characterCountOfCurrentWord + 1
 
-      characterCountOfCurrentWord = 0
-      word = ""
-    } else if
-      committedCharacterCount+runewidth.StringWidth(word)+characterWidth >= maximumWidth {
+			characterCountOfCurrentWord = 0
+			word = ""
+		} else if committedCharacterCount+runewidth.StringWidth(word)+characterWidth >= maximumWidth {
 
-      // giant word - we need to print part of it
-      if characterCountOfCurrentWord + characterWidth >= maximumWidth {
-        wordList.PushBack(word)
-        word = ""
-        characterCountOfCurrentWord = 0
-      }
+			// giant word - we need to print part of it
+			if characterCountOfCurrentWord+characterWidth >= maximumWidth {
+				wordList.PushBack(word)
+				word = ""
+				characterCountOfCurrentWord = 0
+			}
 
-      word += string(character)
-      characterCountOfCurrentWord += characterWidth
+			word += string(character)
+			characterCountOfCurrentWord += characterWidth
 
-      leftoverSpace := 0
-      if justifyText {
-        leftoverSpace = int(
-          math.Min(
-            float64(maximumWidth-(committedCharacterCount)),
-            float64(spaceCount)*1.25))
-      }
-      line := DrainLine(*wordList, spaceCount, leftoverSpace)
-      out += line
-      out += "\n"
-      out += indentString
-      spaceCount = 0
-      committedCharacterCount = 0
-      wordList = list.New()
-    } else {
-      characterCountOfCurrentWord += characterWidth
-      word += string(character)
-    }
-  }
-  line := DrainLine(*wordList, 0, 0)
-  out += line
-  out += word
-  return out
+			leftoverSpace := 0
+			if justifyText {
+				leftoverSpace = int(
+					math.Min(
+						float64(maximumWidth-(committedCharacterCount)),
+						float64(spaceCount)*1.25))
+			}
+			line := DrainLine(*wordList, spaceCount, leftoverSpace)
+			out += line
+			out += "\n"
+			out += indentString
+			spaceCount = 0
+			committedCharacterCount = 0
+			wordList = list.New()
+		} else {
+			characterCountOfCurrentWord += characterWidth
+			word += string(character)
+		}
+	}
+	line := DrainLine(*wordList, 0, 0)
+	out += line
+	out += word
+	return out
 }
